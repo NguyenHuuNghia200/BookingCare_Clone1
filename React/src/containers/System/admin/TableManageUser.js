221326966ac4ec3f63e67a4202ebde271f3890a8
NguyenHuuNghia200 <nghiats200@gmail.com>
2023-02-05 14:17:57 +0700

    5/2

--- React/src/containers/System/admin/TableManageUser.js
+++ React/src/containers/System/admin/TableManageUser.js
@@ -3,6 +3,21 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import '../admin/TableManageUser.scss'
 import * as actions from '../../../store/actions'
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
 
 
 
@@ -36,58 +51,64 @@ class TableManageUser extends Component {
         this.props.deleteUser(dataid.id)
     }
     handleEditUser = (inputuser) => {
-        console.log(inputuser.image,'-----')
+        console.log(inputuser.image, '-----')
         this.props.handleEditUserfromParent(inputuser)
     }
     render() {
         let arrayUsers = this.state.userRedux
 
         return (
-            <div className='users-container'>
-                <div className='users-table mt-4 '>
-                    <table id="TableManageUser">
-                        <tbody>
-                            <tr>
-                                <th>Email</th>
-                                <th>FirstName</th>
-                                <th>LastName</th>
-                                <th>sdt</th>
-                                <th>Address</th>
-                                <th>Actions</th>
-                            </tr>
+            <>
+                <div className='users-container'>
+                    <div className='users-table mt-4 '>
+                        <table id="TableManageUser">
+                            <tbody>
+                                <tr>
+                                    <th>Email</th>
+                                    <th>FirstName</th>
+                                    <th>LastName</th>
+                                    <th>sdt</th>
+                                    <th>Address</th>
+                                    <th>Actions</th>
+                                </tr>
 
-                            {arrayUsers && arrayUsers.length > 0 &&
-                                arrayUsers.map((item, index) => {
+                                {arrayUsers && arrayUsers.length > 0 &&
+                                    arrayUsers.map((item, index) => {
 
-                                    return (
-                                        <tr key={index}>
-                                            <td>{item.email}</td>
-                                            <td>{item.firstName}</td>
-                                            <td>{item.lastName}</td>
-                                            <td>{item.phonenumber}</td>
-                                            <td>{item.address}</td>
-                                            <td className="">
-                                                <button className='btn-edit'
-                                                    onClick={() => this.handleDeleteUser(item)}>
-                                                    <i className="fas fa-trash"></i>
-                                                </button>
-                                                <button className='btn-delete'
-                                                    onClick={() => this.handleEditUser(item)}>
-                                                    <i className="fas fa-pencil-alt"></i>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
+                                        return (
+                                            <tr key={index}>
+                                                <td>{item.email}</td>
+                                                <td>{item.firstName}</td>
+                                                <td>{item.lastName}</td>
+                                                <td>{item.phonenumber}</td>
+                                                <td>{item.address}</td>
+                                                <td className="">
+                                                    <button className='btn-edit'
+                                                        onClick={() => this.handleDeleteUser(item)}>
+                                                        <i className="fas fa-trash"></i>
+                                                    </button>
+                                                    <button className='btn-delete'
+                                                        onClick={() => this.handleEditUser(item)}>
+                                                        <i className="fas fa-pencil-alt"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
 
 
-                    </table>
+                        </table>
 
+                    </div>
                 </div>
-            </div>
-
+            {/* export default props => {
+            return (
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+                );
+        }; */}
+            </>
         );
     }
 
